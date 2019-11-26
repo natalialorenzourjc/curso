d41c797e3c32cf2febd8da3581f42e0279e66909
natalia.lorenzo <natalia.lorenzo@urjc.es>
2019-11-26 14:18:49 +0100

    version 26/11 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { InicioComponent } from './inicio/inicio.component';
 import { SegundoComponent } from './segundo/segundo.component';
 import {FormsModule} from '@angular/forms';
 import { TercerComponent } from './tercer/tercer.component';
+import {MatIconModule, MatMenuModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CuatroComponent } from './cuatro/cuatro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     SegundoComponent,
-    TercerComponent
+    TercerComponent,
+    CuatroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatMenuModule,
+  MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cuatro/model/usuario.model.ts
@@ -0,0 +1,13 @@
+export class usuario {
+
+
+constructor( login:string, nombre: string, apellidos:string){
+  this.login = login;
+  this.nombre= nombre;
+  this.apellidos = apellidos;
+
+}
+login: string;
+nombre: string;
+apellidos: string;
+}
